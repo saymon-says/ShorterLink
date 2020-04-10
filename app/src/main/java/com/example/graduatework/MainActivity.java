4d79358d2ff2a11dc7bf2785d568235db10d33b4
Saymon <kroxotylik@gmail.com>
2020-04-10 08:54:08 +0300

    Добавил дизайн и простой обработчик кнопки (Toast)

--- app/src/main/java/com/example/graduatework/MainActivity.java
+++ app/src/main/java/com/example/graduatework/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.graduatework;
 
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Toast.makeText(this,"hello!", Toast.LENGTH_SHORT).show();
     }
 }
