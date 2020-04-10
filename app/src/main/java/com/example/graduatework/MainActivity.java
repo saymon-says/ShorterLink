ba2e63856491d793f3adc96b84ced99846dd98ea
Saymon <kroxotylik@gmail.com>
2020-04-10 10:02:29 +0300

    Добавил реализацию вывода в простой listView одной строки.

--- app/src/main/java/com/example/graduatework/MainActivity.java
+++ app/src/main/java/com/example/graduatework/MainActivity.java
@@ -1,30 +1,36 @@
 package com.example.graduatework;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    ArrayList<String> linkedList = new ArrayList<>();
+    EditText short_link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listview = findViewById(R.id.list_of_short_link);
+        short_link = findViewById(R.id.short_link);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, linkedList);
+        listview.setAdapter(adapter);
     }
 
     public void onClick(View v) {
-        Toast.makeText(this,"hello!", Toast.LENGTH_SHORT).show();
+        linkedList.add(short_link.getText().toString());
+        adapter.notifyDataSetChanged();
+        short_link.setText("");
     }
 }
